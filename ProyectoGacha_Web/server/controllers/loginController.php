1c59ec014ba6a1d98cb73b110c59bed72ecfb813
elPiveDc <Mateo23Dc@gmail.com>
2025-06-28 00:36:17 -0500

    Eliminar algunas archos y acomodar problama con BD,

--- ProyectoGacha_Web/server/controllers/loginController.php
+++ ProyectoGacha_Web/server/controllers/loginController.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once("/ProyectoGacha_Web/server/db/connection.php");
+include_once("../db/connection.php");
 
 $correo = $_POST['correo'];
 $contraseña = $_POST['contraseña'];
